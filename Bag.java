65c53542e2cc18bab65f0bb414c05acf5417b69c
helenab1 <31667934+helenab1@users.noreply.github.com>
2020-01-07 21:36:02 -0800

    created bag

---
+++ Bag.java
@@ -0,0 +1,29 @@
+package Game;
+import java.util.*;
+import stdlib.StdOut;
+
+//Java program select a random element from List 
+
+import java.util.ArrayList; 
+import java.util.List; 
+import java.util.concurrent.ThreadLocalRandom; 
+
+//Java program select a random element from array 
+
+import java.util.ArrayList; 
+import java.util.List; 
+import java.util.Random; 
+
+public interface Bag {
+	boolean add(Object item);
+	boolean remove(Object item);
+	boolean contains(Object item);
+	int numItems();
+	Object grab();
+	Object[] toArray();
+	
+	public static void main (String[] args) {
+		
+	}
+}
+
